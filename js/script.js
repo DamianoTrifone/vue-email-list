2b46bd637c77047fdacca890a5d8e715ae1a4d39
DamianoTrifone <trifone.damiano@gmail.com>
2021-05-27 17:02:49 +0200

    Scheletro e CDN Vue

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+var app = new Vue({
+
+    el: "#root",
+    data: {
+        
+    }
+
+})
